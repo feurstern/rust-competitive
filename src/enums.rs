b0223ba0f6eb147df2eda8e097edfbc0c88a4590
feurstern <naziincolour123@gmail.com>
2025-07-13 14:46:27 +0700

    #1 implemented advance enum within the function scope to determine the result of student exam"

--- src/enums.rs
+++ src/enums.rs
@@ -1,28 +1,53 @@
+
+
 enum ExaminationResult {
     Passed(String),
     NotPassed(String),
 }
 
-fn determine_student_result(scores: (i32, i32, i32)) -> ExaminationResult {
-    let result = (scores.0 + scores.1 + scores.2) / 3;
+#[derive(Debug)]
+enum AlphabetScore {
+    A,
+    B,
+    C,
+    D,
+    E,
+}
 
-    match result {
+fn determine_student_result(scores: (i32, i32, i32)) -> (ExaminationResult, AlphabetScore) {
+    let average = (scores.0 + scores.1 + scores.2) / 3;
+
+    let exam_result = match average {
         70..=100 => ExaminationResult::Passed(String::from("Passed")),
         _ => ExaminationResult::NotPassed(String::from("Not passed")),
+    };
+
+    let grade = determine_student_alphabet_result(average);
+
+    (exam_result, grade)
+}
+
+fn determine_student_alphabet_result(average: i32) -> AlphabetScore {
+    match average {
+        10..=49 => AlphabetScore::D,
+        50..=74 => AlphabetScore::C,
+        75..=84 => AlphabetScore::B,
+        85..=100 => AlphabetScore::A,
+        _ => AlphabetScore::E,
     }
 }
 
-fn check_result(state: &ExaminationResult) {
-    match state {
-        ExaminationResult::Passed(p) => println!("{}", p),
-        ExaminationResult::NotPassed(f) => println!("{}", f),
+fn print_result(result: ExaminationResult, grade: AlphabetScore) {
+    match result {
+        ExaminationResult::Passed(msg) => println!("✅ {} Grade: {:?}", msg, grade),
+        ExaminationResult::NotPassed(msg) => println!("❌ {} Grade: {:?}", msg, grade),
     }
 }
 
 pub fn enums_print() {
-    let student_score_1 = (80, 80, 70);
+    let student_score_1 = (80, 80, 30);
 
-    let student_score_state = determine_student_result(student_score_1);
+    let (result, grade) = determine_student_result(student_score_1);
 
-    check_result(&student_score_state);
+    print_result(result, grade);
 }
