fa0da25ffa949f810603a77279256246605743b0
feurstern <naziincolour123@gmail.com>
2025-07-20 17:31:17 +0700

    #3 refactor the code by using match expression

--- src/is_this_triangle.rs
+++ src/is_this_triangle.rs
@@ -1,12 +1,11 @@
 pub fn is_this_triangle_display() {
-    let result = is_triangle(4, 2, 1);
+    let result = is_triangle(7, 10, 4);
     println!("is this triangle:{}", result);
 }
 
-fn is_triangle(a: u32, b: u32, c: u32) -> bool {
-    if a <= 0 || b <= 0 || c <= 0 {
-        false
-    } else {
-        a + b > c && a + c > b && b + c > a
+fn is_triangle(a: i32, b: i32, c: i32) -> bool {
+    match (a > 0, b > 0, c > 0) {
+        (true, true, true) => a + b > c && a + c > b && b + c > a,
+        _ => false,
     }
 }
