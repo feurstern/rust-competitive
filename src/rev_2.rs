bb85e0d4f9fce8813639dfb1e1b54304abc98fd4
feurstern <naziincolour123@gmail.com>
2025-07-22 19:04:24 +0700

    #4 rev digit to vector

--- src/rev_2.rs
+++ src/rev_2.rs
@@ -6,7 +6,10 @@ pub fn rev_2_display() {
     println!("enter the value of n");
     let num = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
 
+    println!("enter the value of x");
+    let x = lines.next().unwrap().unwrap().parse::<u32>().unwrap();
     println!("factorial result :{}", rev_factorial(num));
+    println!("vector {:?}", convert_digit_to_vector(x));
 }
 
 fn rev_factorial(num: i32) -> i32 {
@@ -14,4 +17,11 @@ fn rev_factorial(num: i32) -> i32 {
         0 => 1,
         _ => num * rev_factorial(num - 1),
     }
+}
+
+fn convert_digit_to_vector(x: u32) -> Vec<u32> {
+    x.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
 }
