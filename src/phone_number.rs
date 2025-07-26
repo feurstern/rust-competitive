6ffeea4fc9bc5c0875da6bbbd226d52562061c22
feurstern <naziincolour123@gmail.com>
2025-07-26 21:25:28 +0700

    #11 added the input for the numbers input

--- src/phone_number.rs
+++ src/phone_number.rs
@@ -1,4 +1,13 @@
+use std::io::{self, BufRead, stdin};
+
 pub fn create_phone_number_display() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let x = lines.next().unwrap().unwrap().parse::<String>().unwrap();
+    let arr = x
+        .chars()
+        .map(|f| f.to_digit(10).unwrap())
+        .collect::<Vec<u8>>();
     println!(
         "result phone number: {}",
         create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
