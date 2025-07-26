7aa24a1c35f2080d084c4979e3765ac78f0eee0f
feurstern <naziincolour123@gmail.com>
2025-07-26 16:43:05 +0700

    #9 added the new function to calculate the total string from the given message.

--- src/spin_word.rs
+++ src/spin_word.rs
@@ -6,12 +6,16 @@ pub fn spin_word_display() {
 
     println!("w:");
     let words = lines.next().unwrap().unwrap().parse::<String>().unwrap();
-
     println!("fn {:?}", spin_word_fn(words));
 }
 
 fn spin_word_fn(words: String) -> String {
     format!("w:{:?}", words.split(' ').collect::<Vec<&str>>())
+}
 
-    // words.split(" ").collect::<Vec<&str>>(}
+fn words_len(words: String) -> Result<i32, String> {
+    match words.trim().is_empty() {
+        true => Err(String::from("Description words must be empty ")),
+        _ => Ok(words.len() as i32),
+    }
 }
