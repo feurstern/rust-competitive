42cffc0d592ea9dc675c95b82126ff3b800767ae
feurstern <naziincolour123@gmail.com>
2025-07-26 20:12:04 +0700

    #9 finished the problem of the

--- src/spin_word.rs
+++ src/spin_word.rs
@@ -1,31 +1,49 @@
-use std::io::{self, BufRead};
+use std::{
+    io::{self, BufRead},
+    vec,
+};
+
+use serde_json::Error;
 
 pub fn spin_word_display() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
-
     println!("w:");
+
     let words = lines.next().unwrap().unwrap().parse::<String>().unwrap();
     println!("fn {:?}", spin_word_fn(&words));
 
-    let len_string = match words_len(words.to_string()) {
-        Ok(n) => n,
-        Err(s) => {
-            println!("Error :{}", s);
-            return;
-        }
-    };
-
-    println!("len_string :{}", len_string)
+    println!("result spin word :{}", spin_word_fn(&words));
 }
 
 fn spin_word_fn(words: &String) -> String {
-    format!("w:{:?}", words.split(' ').collect::<Vec<&str>>())
+    words
+        .split_whitespace()
+        .map(|w| {
+            if w.len() >= 5 {
+                w.chars().rev().collect::<String>()
+            } else {
+                w.to_string()
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 fn words_len(words: String) -> Result<i32, String> {
-    match words.trim().is_empty() {
-        true => Err(String::from("Description words must be empty ")),
+    match words.len() {
+        0 => Err(String::from("Description must be not empty")),
         _ => Ok(words.len() as i32),
     }
+}
+
+fn spin_words(words: &str) -> String {
+    words
+        .split_ascii_whitespace()
+        .map(|word| match word.len() >= 5 {
+            true => word.chars().rev().collect(),
+            false => word.to_string(),
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
 }
